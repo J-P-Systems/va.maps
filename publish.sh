14b7c79d25ded671c4397b7ab66d4dd5f988d3a0
Michael van der Zel <michael.vanderzel@gmail.com>
2024-09-20 15:08:24 +0200

    Remove large zipfiles

--- publish.sh
+++ publish.sh
@@ -6,6 +6,7 @@ echo "Copy new content FSH input files..."
 cp ../jpsys-maps-ig-sushi/input/fsh/* fsh
 echo "Copy new content IG publisher output..."
 cp -r ../jpsys-maps-ig-sushi/output/* docs
+for i in `find docs  -size +25M`; do rm $i; done
 echo "Commit to git repo..."
 git add fsh 
 git add docs
