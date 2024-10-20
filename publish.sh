80b63af8ad0ebebfa20a186f08d894806ef19f5c
Michael van der Zel <michael.vanderzel@gmail.com>
2024-10-20 11:21:35 +0200

    Fix

--- publish.sh
+++ publish.sh
@@ -8,7 +8,6 @@ echo "Copy new content IG publisher output..."
 cp -r ../jpsys-maps-ig-sushi/output/* docs
 for i in `find docs  -size +25M`; do rm $i; done
 echo "Commit to git repo..."
-git add fsh 
 git add docs
 git commit -a -m "new version"
 echo "Push to git repo..."
